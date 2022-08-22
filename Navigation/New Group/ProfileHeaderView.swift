8b78338fea7a0c5b924f4f1205304ee34fe9bd7c
margarita.isaeva <margarita.isaeva@corp.mail.ru>
2022-08-22 17:06:12 +0300

    poins 1-4

---
+++ Navigation/New Group/ProfileHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by m.isaeva on 17.08.2022.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+
+
+}
---
+++ Navigation/New Group/ProfileViewController2.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController2.swift
+//  Navigation
+//
+//  Created by m.isaeva on 17.08.2022.
+//
+
+import UIKit
+
+class ProfileViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+//        view.addSubview(<#T##view: UIView##UIView#>)
+    }
+    
+    override func viewWillLayoutSubviews() {
+//        <#code#>
+    }
+    
+//    ProfileHeaderView
+//    subview.frame = mainView.bounds
+
+}
