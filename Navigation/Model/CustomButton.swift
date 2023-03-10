10ac3b9080abf2cf1ff4f0728d9b055a814d2e7c
Margarita Isaeva <ma-i-isaeva@yandex-team.ru>
2023-03-10 23:45:46 +0300

    действие по нажатию кнопки

--- Navigation/Model/CustomButton.swift
+++ Navigation/Model/CustomButton.swift
@@ -13,13 +13,21 @@ class CustomButton: UIButton {
     
     let title: String
     let titleColor: UIColor
+
     init(title: String, titleColor: UIColor) {
         self.title = title
         self.titleColor = titleColor
         super.init(frame: .zero)
+        addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+     
+    @objc
+        private func buttonTapped(_ sender: UIButton) {
+            print("Custom button tapped")
+        }
 }
